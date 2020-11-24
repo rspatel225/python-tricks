85a3f1c83a606274d590faacb285635264f3b28a
Ravi Patel <ravipatel225@gmail.com>
2020-11-24 16:42:39 -0600

    comma placement

---
+++ chapter-2/2.2-comma.py
@@ -0,0 +1,10 @@
+# Hard to tell changes in git diff if one line
+bulls = ['jordan', 'pippen', 'rodman']
+
+new_bulls = [
+    'white',
+    'lavine',
+    'markkanen',
+    # Add a comma after the last element
+    'carter',
+]
