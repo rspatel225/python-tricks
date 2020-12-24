e9e5519c997c810f4cebcede8c3eaa5909fdfb9b
Ravi Patel <ravipatel225@gmail.com>
2020-12-24 09:52:19 -0600

    return None

---
+++ chapter-3/3.6_none.py
@@ -0,0 +1,18 @@
+def correct_number(number):
+    if number == 10:
+        return True
+    # implicit return None
+
+def taller(h1, h2):
+    if h1 > h2:
+        return True
+    # equivilant to return None
+    return
+
+def math():
+    1 + 1
+    return None
+
+print(correct_number(21))
+print(taller(1, 10))
+print(math())
