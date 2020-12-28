991fbad1bc0e81d48e45cc1b4c72e7ef4ed95c70
Ravi Patel <ravipatel225@gmail.com>
2020-12-28 14:19:45 -0600

    copies

---
+++ chapter-4/4.4_copies.py
@@ -0,0 +1,26 @@
+import copy
+
+x = [
+    [1, 2, 3],
+    ['a', 'b', 'c'],
+    'one layer'
+]
+
+# a pointer to x
+y = x
+# a shallow copy of x
+z = list(x)
+a = copy.deepcopy(x)
+
+print(f'{y = }')
+print(f'{z = }')
+print(f'{a = }')
+
+x[0][2] = 100
+x[2] = 'changed'
+x.append(['hello, world'])
+print('Made changes to x')
+
+print(f'{y = }')
+print(f'{z = }')
+print(f'{a = }')
