8d08e20d867c94895c71e7af8513cf04cff0a1e5
Ravi Patel <ravipatel225@gmail.com>
2021-01-06 11:03:38 -0600

    named tuples part 2

--- chapter-4/4.6_named_tuples.py
+++ chapter-4/4.6_named_tuples.py
@@ -13,3 +13,9 @@ PowerCard = namedtuple('PowerCard', Card._fields + ('power',))
 
 power_card = PowerCard(8, 'Hearts', 'Next player draws two cards.')
 print(power_card.power)
+
+your_card = Card._make([2, 'Diamonds'])
+print(f'{your_card._asdict() = }')
+new_card = your_card._replace(rank=12)
+print(f'{new_card = }')
+
